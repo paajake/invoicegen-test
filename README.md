b26f78820c27c77abbbb85225903062a8cb486c9
Paa J A K E <paajake@gmail.com>
2020-02-14 20:41:16 +0000

    Added ClearDB config to for Heroku

---
+++ README.md
@@ -0,0 +1,9 @@
+![InvoiceGen Logo](https://raw.githubusercontent.com/paajake/invoicegen-test/master/public/images/logo/logo.png)
+
+#**Invoice**Gen
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/b44d1d29abdf4cbd9a2ac359b4c20425)](https://www.codacy.com?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=paajake/invoicegen-test&amp;utm_campaign=Badge_Grade)
+[![Build Status](https://travis-ci.org/paajake/invoicegen-test.svg?branch=master)](https://travis-ci.org/paajake/invoicegen-test)
+[![codecov](https://codecov.io/gh/paajake/invoicegen-test/branch/master/graph/badge.svg)](https://codecov.io/gh/paajake/invoicegen-test)
+
+    
--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -45,13 +52,29 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'heroku' => [
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
